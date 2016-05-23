33d583a365d37d2e7012e74a8fe340b9436bc124
Jeson <1655087079@qq.com>
2016-05-23 15:37:44 +0800

    使用TabHost控件，完成基本布局

--- app/src/main/java/com/example/angluswang/clock/MainActivity.java
+++ app/src/main/java/com/example/angluswang/clock/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.angluswang.clock;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TabHost;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTabHost = (TabHost) findViewById(R.id.tabHost);
+        mTabHost.setup();
+
+        mTabHost.addTab(mTabHost.newTabSpec("tabTime").setIndicator("时钟").setContent(R.id.tabTime));
+        mTabHost.addTab(mTabHost.newTabSpec("tabAlarm").setIndicator("闹钟").setContent(R.id.tabAlarm));
+        mTabHost.addTab(mTabHost.newTabSpec("tabTimer").setIndicator("计时器").setContent(R.id.tabTimer));
+        mTabHost.addTab(mTabHost.newTabSpec("tabStopWatch").setIndicator("秒表").setContent(R.id.tabStopWatch));
     }
+
+    private TabHost mTabHost;
 }
